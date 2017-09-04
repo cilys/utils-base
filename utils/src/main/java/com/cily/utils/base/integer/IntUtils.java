ebe98140b2a20490c866f50aae2206f3e4b45d68
cilycode <1321851830@qq.com>
2017-09-04 14:19:27 +0800

    计算int类型数据长度

---
+++ utils/src/main/java/com/cily/utils/base/integer/IntUtils.java
@@ -0,0 +1,24 @@
+package com.cily.utils.base.integer;
+
+/**
+ * Created by 13218 on 2017/8/14.
+ */
+public class IntUtils {
+
+    private final static int [] sizeTable = { 9, 99, 999, 9999, 99999, 999999, 9999999,
+            99999999, 999999999, Integer.MAX_VALUE };
+
+    // Requires positive x
+    public static int stringSize(int x) {
+        if (x < 0){
+            x = Math.abs(x);
+        }
+        for (int i=0; ; i++)
+            if (x <= sizeTable[i])
+                return i+1;
+    }
+
+//    public static void main(String[] args){
+//        System.out.println("***" + stringSize(-1999999999));
+//    }
+}
